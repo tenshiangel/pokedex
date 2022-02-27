764772d4c403f75b5d04ce2a60d4b167d8229301
Stephen Balansag <vipers083@gmail.com>
2022-02-27 21:59:55 +0800

    CSS and JS dependencies

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,16 +1,27 @@
 import AppComponent from './components/AppComponent';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import LandingPage from './pages/LandingPage';
+import Profile from './pages/Profile';
 
 export default {
     mode: 'history',
     routes: [
         {
-            path: 'home',
-            name: 'home',
-            component: AppComponent
+            path: '/',
+            redirect: '/login'
         },
         {
-            path: '/',
-            component: AppComponent,
-        }
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/register',
+            component: Registration
+        },
+        {
+            path: '/home',
+            component: AppComponent
+        },
     ],
 }
