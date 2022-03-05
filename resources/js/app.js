08faa6978951edc0dbf7e3a932f84cb2cca7451b
Stephen Balansag <vipers083@gmail.com>
2022-03-05 20:18:14 +0800

    CSS and JS updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,8 +22,6 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('app-component', require('./components/AppComponent.vue').default);
 Vue.component('pokemon-card', require('./components/pokemon/PokemonCard.vue').default);
 
 Vue.use(BootstrapVue);
